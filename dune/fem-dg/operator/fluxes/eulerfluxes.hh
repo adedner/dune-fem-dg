f08c121a42f705e11712da93e475d2d750a6f743
Robert Klöfkorn <robertk@ucar.edu>
2011-05-25 22:06:16 +0000

    added euler analytical flux impl

--- dune/fem-dg/operator/fluxes/eulerfluxes.hh
+++ dune/fem-dg/operator/fluxes/eulerfluxes.hh
@@ -5,12 +5,207 @@
 #include <string>
 #include <cmath>
 
+#include <dune/common/fvector.hh>
+
 // dune-grid includes
 #if WELLBALANCE
 #include <dune/grid/common/genericreferenceelements.hh>
 #endif
 
 #include <dune/fem-dg/operator/fluxes/rotator.hh>
+
+///////////////////////////////////////////////////////////
+//
+//  EulerAnalyticalFlux 
+//
+///////////////////////////////////////////////////////////
+
+template <int dimDomain>
+class EulerAnalyticalFlux
+{
+  enum { e = dimDomain+1};
+  enum { dimRange = dimDomain+2 };
+ public:
+  typedef double FieldType;
+  typedef Dune :: FieldVector< FieldType, dimRange> RangeType;
+  typedef Dune :: FieldMatrix< FieldType, dimRange,dimDomain> FluxRangeType;
+  typedef Dune :: FieldVector< FieldType, dimDomain> DomainType;
+  inline void analyticalFlux(const FieldType gamma,
+                             const RangeType& u,
+                             FluxRangeType& f) const;
+  inline  void jacobian(const FieldType gamma,
+                        const RangeType& u,
+                        const FluxRangeType& du,
+                        RangeType& A) const;
+
+  inline FieldType rhoeps(const RangeType& u) const;
+  inline FieldType pressure(const FieldType gamma,const RangeType& u) const;
+  inline FieldType maxSpeed(const FieldType gamma, const DomainType& n,
+                         const RangeType& u) const;
+};
+
+// ***********************
+template <>
+inline
+EulerAnalyticalFlux<1>::FieldType EulerAnalyticalFlux<1>::rhoeps(const RangeType& u) const 
+{
+  assert( u[0] >= 1e-10 );
+  return u[e]-0.5*(u[1]*u[1])/u[0];
+}
+template <>
+inline
+EulerAnalyticalFlux<1>::FieldType EulerAnalyticalFlux<1>::pressure(const FieldType gamma,const RangeType& u) const {
+  FieldType rhoe = rhoeps(u);
+  assert(rhoe>1e-10);
+  return (gamma-1)*rhoe;
+}
+template <>
+inline
+void EulerAnalyticalFlux<1>::analyticalFlux(const FieldType gamma,
+          const RangeType& u,
+          FluxRangeType& f) const {
+  assert(u[0]>1e-10);
+  const FieldType p = pressure(gamma, u);
+  f[0][0] = u[1];
+  f[1][0] = u[1]/u[0]*u[1]+p;
+  f[e][0] = u[1]/u[0]*(u[e]+p);
+}
+template <>
+inline
+EulerAnalyticalFlux<1>::FieldType EulerAnalyticalFlux<2>::rhoeps(const RangeType& u) const 
+{
+  assert( u[0] >= 1e-10 );
+  return u[e]-0.5*(u[1]*u[1]+u[2]*u[2])/u[0];
+}
+template <>
+inline
+EulerAnalyticalFlux<1>::FieldType EulerAnalyticalFlux<2>::pressure(const FieldType gamma,const RangeType& u) const 
+{
+  const FieldType re = rhoeps(u);
+  assert(re>=1e-10);
+  return (gamma-1)*re;
+}
+template <>
+inline
+void EulerAnalyticalFlux<2>::analyticalFlux(const FieldType gamma,
+          const RangeType& u,
+          FluxRangeType& f) const 
+{
+  assert( u[0] >= 1e-10 );
+  const FieldType v[2] = {u[1]/u[0],u[2]/u[0]};
+  const FieldType p = pressure( gamma, u );
+  const FieldType ue_p = (u[e]+p);
+  f[0][0] = u[1];            f[0][1] = u[2];
+  f[1][0] = v[0]*u[1]+p;     f[1][1] = v[1]*u[1];
+  f[2][0] = v[0]*u[2];       f[2][1] = v[1]*u[2]+p;
+  f[e][0] = v[0]*ue_p;       f[e][1] = v[1]*ue_p;
+}
+
+template <>
+inline
+EulerAnalyticalFlux<1>::FieldType EulerAnalyticalFlux<3>::rhoeps(const RangeType& u) const 
+{
+  assert( u[0] >= 1e-10 );
+  return u[e]-0.5*(u[1]*u[1]+u[2]*u[2]+u[3]*u[3])/u[0];
+}
+template <>
+inline
+EulerAnalyticalFlux<1>::FieldType EulerAnalyticalFlux<3>::pressure(const FieldType gamma,const RangeType& u) const {
+  assert(u[0]>1e-10);
+  const FieldType rhoe = rhoeps(u);
+  assert( rhoe>1e-10 );
+  return (gamma-1)*rhoe;
+}
+template <>
+inline
+void EulerAnalyticalFlux<3>::analyticalFlux(const FieldType gamma,
+          const RangeType& u,
+          FluxRangeType& f) const 
+{
+  assert(u[0]>1e-10);
+  const FieldType p = pressure(gamma,u);
+  // get the velocity 
+  const FieldType v[3] = {u[1]/u[0], u[2]/u[0], u[3]/u[0]};
+  const FieldType ue_p = (u[e]+p);
+  
+  f[0][0]=u[1];          f[0][1]=u[2];           f[0][2]=u[3];
+  f[1][0]=v[0]*u[1]+p;   f[1][1]=v[1]*u[1];      f[1][2]=v[2]*u[1];
+  f[2][0]=v[0]*u[2];     f[2][1]=v[1]*u[2]+p;    f[2][2]=v[2]*u[2];
+  f[3][0]=v[0]*u[3];     f[3][1]=v[1]*u[3];      f[3][2]=v[2]*u[3]+p;
+  f[e][0]=v[0]*ue_p;     f[e][1]=v[1]*ue_p;      f[e][2]=v[2]*ue_p;
+}
+template <>
+inline
+void EulerAnalyticalFlux<2>::jacobian(const FieldType gamma,
+      const EulerAnalyticalFlux<2>::RangeType& u, 
+      const EulerAnalyticalFlux<2>::FluxRangeType& du,
+      EulerAnalyticalFlux<2>::RangeType& A) const 
+{
+  assert(u[0]>1e-10);
+  FieldType v[2] = {u[1]/u[0],u[2]/u[0]};
+  
+  A[0] = du[1][0] + du[1][1];
+  A[1] = du[0][0]*((gamma-3.0)/2.0*v[0]*v[0] +(gamma-1.0)/2.0*v[1]*v[1])-
+    du[0][1]*v[0]*v[1];
+  A[1] += du[1][0]*(3.0-gamma)*v[0] + du[1][1]*v[1];
+  A[1] += du[2][0]*(1.0-gamma)*v[1] + du[2][1]*v[0];
+  A[1] += du[3][0]*(gamma-1.0);
+  A[2] = du[0][1]*((gamma-3.0)/2.0*v[1]*v[1] +(gamma-1.0)/2.0*v[0]*v[0])-
+    du[0][0]*v[0]*v[1];
+  A[2] += du[1][1]*(1.0-gamma)*v[0] + du[1][0]*v[1];
+  A[2] += du[2][1]*(3.0-gamma)*v[1] + du[2][0]*v[0];
+  A[2] += du[3][1]*(gamma-1.0);
+  A[3] = du[0][0]*(-gamma*u[3]*v[0]/u[0]+(gamma-1.0)*v[0]*(v[0]*v[0]+v[1]*v[1]))+
+    du[0][1]*(-gamma*u[3]*v[1]/u[0]+(gamma-1.0)*v[1]*(v[0]*v[0]+v[1]*v[1]));
+  A[3] += du[1][0]*(gamma*u[3]/u[0]-(gamma-1.0)/2.0*(3.0*v[0]*v[0]+v[1]*v[1]))-
+    du[1][1]*(gamma-1.0)*v[0]*v[1];
+  A[3] += -du[2][0]*(gamma-1.0)*v[0]*v[1]+
+    du[2][1]*(gamma*u[3]/u[0]-(gamma-1.0)/2.0*(v[0]*v[0]+3.0*v[1]*v[1]));
+  A[3] += du[3][0]*gamma*v[0] + du[3][1]*gamma*v[1];
+}
+template <>
+inline
+EulerAnalyticalFlux<1>::FieldType EulerAnalyticalFlux<1>::maxSpeed(const FieldType gamma,
+            const DomainType& n,
+            const RangeType& u) const {
+  assert(u[0]>1e-10);
+  FieldType u_normal = u[1]*n[0] / u[0];
+  FieldType p = pressure(gamma,u);
+  const FieldType c2 = gamma*p/u[0]*n.two_norm2();
+  assert(c2>1e-10);
+  //return std::abs(u_normal) + std::sqrt(c2);
+  return std::abs(u_normal) + std::sqrt(c2);
+}
+template <>
+inline
+EulerAnalyticalFlux<1>::FieldType EulerAnalyticalFlux<2>::maxSpeed(const FieldType gamma,
+            const DomainType& n,
+            const RangeType& u) const 
+{
+  assert( u[0] > 1e-10 );
+  FieldType u_normal = (u[1]*n[0]+u[2]*n[1]) / u[0];
+  FieldType p = pressure(gamma,u);
+  FieldType c2 = gamma * p/ u[0] * n.two_norm2();
+  assert( c2 > 1e-10 );
+  return std::abs(u_normal) + std::sqrt(c2);
+}
+template <>
+inline
+EulerAnalyticalFlux<1>::FieldType EulerAnalyticalFlux<3>::maxSpeed(const FieldType gamma,
+            const DomainType& n,
+            const RangeType& u) const 
+{
+  assert(u[0]>1e-10);
+  const FieldType u_normal = (u[1]*n[0]+u[2]*n[1]+u[3]*n[2]) / u[0];
+  const FieldType p = pressure(gamma,u);
+  FieldType c2 = gamma*p/u[0]*n.two_norm2();
+  assert(c2>1e-10);
+  return std::abs(u_normal) + std::sqrt(c2);
+}
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
 
 // local includes
 namespace EULERNUMFLUX
