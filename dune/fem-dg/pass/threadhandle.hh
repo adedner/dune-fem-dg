d43c555ae39382e0a085bff360499971741b99f2
Robert Kloefkorn <robertk@ucar.edu>
2013-10-10 13:55:49 -0600

    add setThreadNumber in the thread section to correctly set the thread's number when thread local storage is used.

--- dune/fem-dg/pass/threadhandle.hh
+++ dune/fem-dg/pass/threadhandle.hh
@@ -105,8 +105,11 @@ class ThreadHandle
         {
           // create a joinable thread 
           pthread_create(&threadId_, 0, &ThreadHandleObject::startThread, (void *) this);
-          // the master thread is also adding the threadnumber for the given ids 
+#if ! HAVE_PTHREAD_TLS
+          // the master thread is adding the threadnumber for the given ids 
+          // if no thread local storage is available
           ThreadManager :: setThreadNumber( threadId_, threadNumber_ );
+#endif
         }
       }
       else 
@@ -168,6 +171,10 @@ class ThreadHandle
     // C style function pointer for the pthread_create call
     static void* startThread(void *obj)
     {
+#if HAVE_PTHREAD_TLS
+      // when thread local storage is available then each thread adds it's thread number and not the master
+      ThreadManager :: setThreadNumber( pthread_self(), ((ThreadHandleObject *) obj)->threadNumber_ );
+#endif
       // do the work
       ((ThreadHandleObject *) obj)->run();
       return 0;
