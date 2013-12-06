0d46a7fc5ce220e98e60122b89834da48153fcd2
Robert Kloefkorn <robertk@ucar.edu>
2013-12-06 12:27:27 +0100

    added advection+diffusion problems.

---
+++ dune/fem-dg/test/advdiff/problemQuasiHeatEqn.hh
@@ -0,0 +1,166 @@
+#ifndef  DUNE_PROBLEM__QUASI_HH__
+#define  DUNE_PROBLEM__QUASI_HH__
+
+#include <dune/common/version.hh>
+
+// dune-fem includes
+#include <dune/fem/misc/linesegmentsampler.hh>
+#include <dune/fem/io/parameter.hh>
+#include <dune/fem/space/common/functionspace.hh>
+
+// local includes
+#include <dune/fem-dg/models/defaultprobleminterfaces.hh>
+
+
+namespace Dune {
+
+/**
+ * @brief describes the initial and exact solution of the advection-diffusion model
+ */
+template <class GridType>                                          /*@LST0S@*/
+struct QuasiHeatEqnSolution : public EvolutionProblemInterface<
+                  Dune::Fem::FunctionSpace< double, double, GridType::dimension, DIMRANGE>,
+                  true >
+{                                                                  /*@LST0E@*/
+public:
+  typedef EvolutionProblemInterface<
+                 Dune::Fem::FunctionSpace< double, double,
+                                           GridType::dimension, DIMRANGE >,
+                 true >                                              BaseType;
+
+  enum{ dimDomain = BaseType :: dimDomain };
+  typedef typename BaseType :: DomainType                            DomainType;
+  typedef typename BaseType :: RangeType                             RangeType;
+  typedef typename BaseType :: JacobianRangeType     JacobianRangeType;
+
+  typedef Fem :: Parameter  ParameterType ;
+
+  /**
+   * @brief define problem parameters
+   */
+  QuasiHeatEqnSolution () :                                                        /*@LST0S@*/
+    BaseType () ,
+    velocity_( 0 ),
+    startTime_( ParameterType::getValue<double>("femhowto.startTime",0.0) ),
+    epsilon_( ParameterType::getValue<double>("femhowto.epsilon") )
+  {                                                             /*@LST0E@*/
+    if ( (DIMRANGE != 1) || (dimDomain != 2) )
+    {
+      std::cout <<"QuasiHeatEqn only supports dimRange=1 and dimDomain=2\n";
+      abort();
+    }
+    std::cout <<"Problem: QuasiHeatEqn" <<epsilon_ << " epsilon \n";
+
+    myName = "QuasiHeatEqn";
+  }
+
+  double diffusion( const RangeType& u, const JacobianRangeType& gradU ) const
+  { 
+    return epsilon_*u;
+  } 
+
+  double startTime() const { return startTime_; }
+
+  double epsilon() const { return epsilon_; }
+
+  /**
+   * @brief getter for the velocity
+   */
+  void velocity(const DomainType& x, DomainType& v) const
+  {
+    v = velocity_;
+  }
+
+  /**
+   * @brief evaluates \f$ u_0(x) \f$
+   */
+  void evaluate(const DomainType& arg, RangeType& res) const         /*@LST0S@@LST0E@*/
+  {
+    evaluate(arg, startTime_, res);
+  }
+
+  /**
+   * @brief evaluate exact solution
+   */
+  void evaluate(const DomainType& arg, const double t, RangeType& res) const /*@LST0S@@LST0E@*/
+  {
+    res = std::sin(arg[0]) * std::sin(arg[1]) * std::exp( -epsilon_*t );
+  }
+
+  bool hasStiffSource() const { return true; }
+  bool hasNonStiffSource() const { return false; }
+
+  /**
+   * @brief evaluate stiff source function
+   */
+  double nonStiffSource( const DomainType& arg, 
+                         const double t,
+                         const RangeType& u,
+                         RangeType& res ) const /*@LST0S@@LST0E@*/
+  {
+    res = 0.;
+
+    // time step restriction from the stiff source term 
+    return 0.;
+  }
+
+  /**
+   * @brief evaluate non stiff source function
+   */
+  double stiffSource( const DomainType& arg,
+                         const double t, 
+                         const RangeType& u, 
+                         RangeType& res ) const /*@LST0S@@LST0E@*/
+  {
+    const double x = arg[0];
+    const double y = arg[1];
+    const double cosx2 = cos(x)*cos(x);
+    const double sinx2 = sin(x)*sin(x);
+    const double cosy2 = cos(y)*cos(y);
+    const double siny2 = sin(y)*sin(y);
+
+    res = (-std::exp(-2.*epsilon_*t )*(cosx2*siny2 + cosy2*sinx2) + 2*u*u - u);
+    res *= epsilon_;
+
+    // time step restriction from the non stiff source term 
+    return 0.0;
+  }
+
+  /**
+   * @brief latex output for EocOutput
+   */
+  std::string description() const
+  {
+    std::ostringstream ofs;
+
+    ofs << "Problem: " << myName
+      << ", Epsilon: " << epsilon_ 
+      << ", End time: " << ParameterType::getValue<double>("femhowto.endTime");
+
+    return ofs.str();
+  }
+
+
+  /*  \brief finalize the simulation using the calculated numerical
+   *  solution u for this problem
+   *  
+   *  \param[in] variablesToOutput Numerical solution in the suitably chosen variables
+   *  \param[in] eocloop Specific EOC loop
+   */
+  template< class DiscreteFunctionType >
+  void finalizeSimulation( DiscreteFunctionType& variablesToOutput,
+                           const int eocloop) const
+  {}
+
+private:
+  DomainType velocity_;
+  double     startTime_;
+
+public:
+  double      epsilon_;
+  std::string myName;
+};
+
+}
+#endif  /*DUNE_PROBLEM_HH__*/
+
---
+++ dune/fem-dg/test/advdiff/problemcreator.hh
@@ -0,0 +1,99 @@
+#ifndef FEMHOWTO_HEATSTEPPER_HH
+#define FEMHOWTO_HEATSTEPPER_HH
+#include <config.h>
+
+// dune-fem includes
+#include <dune/fem/io/parameter.hh>
+
+#include <dune/grid/io/file/dgfparser/dgfparser.hh>
+#include <dune/fem-dg/operator/fluxes/eulerfluxes.hh>
+
+// local includes
+#include <dune/fem-dg/operator/fluxes/diffusionflux.hh>
+#include "problem.hh"
+#include "problemQuasiHeatEqn.hh"
+#include "deformationalflow.hh"
+#include "models.hh"
+
+
+template< class GridType > 
+struct ProblemGenerator 
+{
+  typedef Dune :: EvolutionProblemInterface<
+                      Dune::Fem::FunctionSpace< double, double, GridType::dimension,
+                      DIMRANGE>,
+                      false > ProblemType;
+  //typedef HeatProblemType ProblemType;
+
+  template< class GridPart >
+  struct Traits
+  {
+    typedef ProblemType  InitialDataType;
+    typedef HeatEqnModel< GridPart, InitialDataType > ModelType;
+    typedef LLFFlux< ModelType > FluxType;
+    //typedef UpwindFlux< ModelType > FluxType;
+    // choice of diffusion flux (see diffusionflux.hh for methods)
+     static const Dune :: DGDiffusionFluxIdentifier PrimalDiffusionFluxId 
+       =  Dune :: method_general ;
+  };
+
+
+  static inline std::string advectionFluxName()
+  {
+#if (FLUX==1)
+    return "LLF";
+#elif (FLUX==2)
+    return "HLL(Dennis)";
+#elif (FLUX==3)
+    return "HLLC(Dennis)";
+#elif (FLUX==4)
+    return "HLL2C";
+#elif (FLUX==5)
+    return "HLL2";
+#elif (FLUX==6)
+    return "HLLEM(Mhd)";
+#endif
+  }
+
+
+  static inline std::string diffusionFluxName()
+  {
+#ifdef EULER
+    return "";
+#elif (defined PRIMALDG)
+    return Dune::Fem::Parameter::getValue< std::string >("dgdiffusionflux.method");
+#else
+    return "LDG";
+#endif
+  }
+
+
+  static inline Dune::GridPtr<GridType> 
+  initializeGrid( const std::string description )
+  {
+    // use default implementation 
+    return initialize< GridType > ( description );
+  }
+
+  static ProblemType* problem()
+  {
+    // choice of explicit or implicit ode solver
+    //static const std::string probString[]  = { "heat" ,"quasi", "deformational" };
+    //int probNr = Dune::Fem::Parameter::getEnum( "femhowto.problem", probString, 0 );
+    //if( probNr == 0 ) 
+    //  return new Dune :: U0< GridType > ();
+    //else if ( probNr == 1 ) 
+    //  return new Dune :: QuasiHeatEqnSolution< GridType > ();
+    //else if ( probNr == 2 ) 
+      return new Dune :: DeformationalFlow< GridType > ();
+    //else 
+    //{
+    //  abort();
+    //  return 0;
+    //}
+  }
+};
+
+//#include <dune/fem-dg/stepper/advectionstepper.hh>
+
+#endif // FEMHOWTO_HEATSTEPPER_HH
