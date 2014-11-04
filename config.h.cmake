fb5a542dfc5b54a8c8a2f9821ce20631f64c2f88
Stefan Girke <stefan.girke@gmx.de>
2014-11-04 19:21:59 +0100

    [cmake] added CMakeLists.txt
    Conflicts:
    	dune/fem-dg/main/main_pol.cc

--- config.h.cmake
+++ config.h.cmake
@@ -1,19 +1,19 @@
-/* begin dune-fem */
+/* begin dune-fem-dg */
 
-/* Define to the version of dune-fem */
+/* Define to the version of dune-fem-dg */
 #define DUNE_FEM_DG_VERSION "${DUNE_FEM_DG_VERSION}"
 
-/* Define to the major version of dune-fem */
+/* Define to the major version of dune-fem-dg */
 #define DUNE_FEM_DG_VERSION_MAJOR ${DUNE_FEM_DG_VERSION_MAJOR}
 
-/* Define to the minor version of dune-fem */
+/* Define to the minor version of dune-fem-dg */
 #define DUNE_FEM_DG_VERSION_MINOR ${DUNE_FEM_DG_VERSION_MINOR}
 
-/* Define to the revision of dune-fem */
+/* Define to the revision of dune-fem-dg */
 #define DUNE_FEM_DG_VERSION_REVISION ${DUNE_FEM_DG_VERSION_REVISION}
 
 
 /* Define if BGQ_L!PREFETCH found */
 #cmakedefine HAVE_BGQ_L1PREFETCH 1
 
-/* end dune-fem */
+/* end dune-fem-dg */
