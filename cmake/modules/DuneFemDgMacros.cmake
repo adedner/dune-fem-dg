58d4e0770e637d7c664bfa5462ff0a33ab17dc5e
Robert K <robertk@posteo.org>
2015-07-18 11:57:50 +0200

    added TargetDistclean.

--- cmake/modules/DuneFemDgMacros.cmake
+++ cmake/modules/DuneFemDgMacros.cmake
@@ -6,4 +6,5 @@ set(SOURCEMAIN  ${FEMDG_MAIN_DIR}/main.cc)
 set(SOURCEONE   ${SOURCEMAIN} ${FEMDG_MAIN_DIR}/main_pol.cc)
 set(SOURCEALL   ${SOURCEMAIN} ${FEMDG_MAIN_DIR}/main_0.cc ${FEMDG_MAIN_DIR}/main_1.cc ${FEMDG_MAIN_DIR}/main_2.cc ${FEMDG_MAIN_DIR}/main_3.cc ${FEMDG_MAIN_DIR}/main_4.cc)
 
+include(TargetDistclean)
 message(AUTHOR_WARNING "TODO: Implement module test.")
