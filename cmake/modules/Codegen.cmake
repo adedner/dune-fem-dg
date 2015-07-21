82a275c6ed4e61b556cd6204381290d820c61c65
Robert K <robertk@posteo.org>
2015-07-21 11:38:06 +0200

    renamed codegen targets.
    ${target}_codegenerator creates the files.
    ${target}_optmized uses the autogenerator files.

--- cmake/modules/Codegen.cmake
+++ cmake/modules/Codegen.cmake
@@ -12,23 +12,23 @@ function(add_code_generate_targets)
       string(REPLACE "compilesources-NOTFOUND" "" compilesources "${compilesources}")
 
       #Add targets and executables
-      add_executable( ${_target}_generatecode ${compilesources} )
-      set_property(TARGET ${_target}_generatecode APPEND PROPERTY COMPILE_DEFINITIONS "NDEBUG;BASEFUNCTIONSET_CODEGEN_GENERATE;${compiledefs}")
-      dune_target_link_libraries( ${_target}_generatecode "${DUNE_LIBS}" )
+      add_executable( ${_target}_codegenerator ${compilesources} )
+      set_property(TARGET ${_target}_codegenerator APPEND PROPERTY COMPILE_DEFINITIONS "NDEBUG;BASEFUNCTIONSET_CODEGEN_GENERATE;${compiledefs}")
+      dune_target_link_libraries( ${_target}_codegenerator "${DUNE_LIBS}" )
 
-      add_executable( ${_target}_compilecode ${compilesources} )
-      set_property(TARGET ${_target}_compilecode APPEND PROPERTY COMPILE_DEFINITIONS "USE_BASEFUNCTIONSET_CODEGEN;${compiledefs}")
-      dune_target_link_libraries( ${_target}_compilecode "${DUNE_LIBS}" )
+      add_executable( ${_target}_optimized ${compilesources} )
+      set_property(TARGET ${_target}_optimized APPEND PROPERTY COMPILE_DEFINITIONS "USE_BASEFUNCTIONSET_CODEGEN;${compiledefs}")
+      dune_target_link_libraries( ${_target}_optimized "${DUNE_LIBS}" )
 
       add_custom_target( ${_target}_generate
-        ${CMAKE_COMMAND} -D RUN_CODEGEN_PROGRAM=${CMAKE_CURRENT_BINARY_DIR}/${_target}_generatecode -D RUN_CODEGEN_PARAMFILE="" -P ${CMAKE_SOURCE_DIR}/cmake/scripts/RunGenerate.cmake )
+        ${CMAKE_COMMAND} -D RUN_CODEGEN_PROGRAM=${CMAKE_CURRENT_BINARY_DIR}/${_target}_codegenerator -D RUN_CODEGEN_PARAMFILE="" -P ${CMAKE_SOURCE_DIR}/cmake/scripts/RunGenerate.cmake )
 
       add_custom_target( ${_target}_codegen )
 
       #Add depenencies
-      add_dependencies( ${_target}_generate ${_target}_generatecode )
-      add_dependencies( ${_target}_compilecode ${_target}_generate )
-      add_dependencies( ${_target}_codegen ${_target}_compilecode )
+      add_dependencies( ${_target}_generate ${_target}_codegenerator )
+      # add_dependencies( ${_target}_compilecode ${_target}_generate )
+      add_dependencies( ${_target}_codegen ${_target}_optimized )
     endif()
   endforeach()
 
