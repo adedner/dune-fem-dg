4094643edb696bccb5abfc934eb11f56f0db2f0a
Stefan Girke <stefan.girke@wwu.de>
2015-09-16 14:36:44 +0200

    use declval to detect types without default constructor

--- dune/fem-dg/misc/typedefcheck.hh
+++ dune/fem-dg/misc/typedefcheck.hh
@@ -2,21 +2,21 @@
 #define DUNE_TYPEDEF_CHECK_HH
 
 
-#define CHECK_TYPEDEF_EXISTS( tname )                                                                \
-    template< class T, bool exists >                                                                \
-    struct tname##_Helper                                                                             \
-    { typedef void type; };                                                                         \
-    template< class T  >                                                                            \
-    struct tname##_Helper< T, true >                                                                  \
-    { typedef typename T::tname type; };                                                           \
-    template< class T >                                                                             \
-    struct tname##s {                                                                                \
-      template <typename TT>                                                                        \
-      static auto apply(TT const&) -> decltype( typename TT::tname(), std::true_type()) {          \
-        return std::true_type();                                                                    \
-      }                                                                                             \
-      static std::false_type apply(...) { return std::false_type(); }                               \
-      typedef typename tname##_Helper< T, decltype( apply( std::declval<T>() ) )::value >::type type; \
-    };                                                                                              \
+#define CHECK_TYPEDEF_EXISTS( tname )                                                                   \
+    template< class T, bool exists >                                                                    \
+    struct tname##_Helper                                                                               \
+    { typedef void type; };                                                                             \
+    template< class T  >                                                                                \
+    struct tname##_Helper< T, true >                                                                    \
+    { typedef typename T::tname type; };                                                                \
+    template< class T >                                                                                 \
+    struct tname##s {                                                                                   \
+      template <typename TT>                                                                            \
+      static auto apply(TT const&) -> decltype( std::declval<typename TT::tname>(), std::true_type()) { \
+        return std::true_type();                                                                        \
+      }                                                                                                 \
+      static std::false_type apply(...) { return std::false_type(); }                                   \
+      typedef typename tname##_Helper< T, decltype( apply( std::declval<T>() ) )::value >::type type;   \
+    };                                                                                                  \
 
 #endif
