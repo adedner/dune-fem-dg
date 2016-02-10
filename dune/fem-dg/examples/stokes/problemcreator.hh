9823b4641028207b199757df7c7385e7247f012b
Stefan Girke <stefan.girke@wwu.de>
2016-02-10 13:18:14 +0100

    new test structure to make "make test" work correctly

---
+++ dune/fem-dg/examples/stokes/problemcreator.hh
@@ -0,0 +1,236 @@
+#ifndef FEMDG_STOKESSTEPPER_HH
+#define FEMDG_STOKESSTEPPER_HH
+#include <config.h>
+
+#include <dune/fem-dg/misc/static_warning.hh>
+
+
+#ifndef GRIDDIM
+#define GRIDDIM 2
+#endif
+
+#ifndef DIMRANGE
+#define DIMRANGE GRIDDIM
+#endif
+
+#ifndef POLORDER
+#define POLORDER 1
+#endif
+
+//--------- HANDLER --------------------------------
+#include <dune/fem-dg/algorithm/handler/sub/diagnostics.hh>
+#include <dune/fem-dg/algorithm/handler/sub/solvermonitor.hh>
+#include <dune/fem-dg/algorithm/handler/sub/additionaloutput.hh>
+#include <dune/fem-dg/algorithm/handler/sub/adapt.hh>
+
+//--------- GRID HELPER ---------------------
+#include <dune/fem-dg/algorithm/gridinitializer.hh>
+#include <dune/fem-dg/examples/poisson/gridinitializer.hh>
+//--------- OPERATOR/SOLVER -----------------
+#include <dune/fem-dg/assemble/primalmatrix.hh>
+#include <dune/fem-dg/operator/dg/operatortraits.hh>
+//--------- FLUXES ---------------------------
+#include <dune/fem-dg/operator/fluxes/euler/fluxes.hh>
+#include <dune/fem-dg/operator/fluxes/advection/fluxes.hh>
+//--------- STEPPER -------------------------
+#include <dune/fem-dg/examples/stokes/stokesalgorithm.hh>
+#include <dune/fem-dg/algorithm/steadystate.hh>
+//--------- EOCERROR ------------------------
+#include <dune/fem-dg/misc/error/l2eocerror.hh>
+#include <dune/fem-dg/misc/error/h1eocerror.hh>
+#include <dune/fem-dg/misc/error/dgeocerror.hh>
+//--------- PROBLEMS ------------------------
+#include "problems.hh"
+//--------- MODELS --------------------------
+#include "models.hh"
+//--------- PROBLEMCREATORSELECTOR ----------
+#include <dune/fem-dg/misc/configurator.hh>
+
+
+
+namespace Dune
+{
+namespace Fem
+{
+
+  static const Galerkin::Enum               galerkinEnum   = Galerkin::Enum::dg;
+  static const Adaptivity::Enum             adaptivityEnum = Adaptivity::Enum::yes;
+  static const DiscreteFunctionSpaces::Enum dfSpaceEnum    = DiscreteFunctionSpaces::Enum::hierarchic_legendre;
+  static const Solver::Enum                 solverEnum     = Solver::Enum::istl;
+  static const AdvectionLimiter::Enum       advLimiterEnum = AdvectionLimiter::Enum::unlimited;
+  static const Matrix::Enum                 matrixEnum     = Matrix::Enum::assembled;
+  static const AdvectionFlux::Enum          advFluxEnum    = AdvectionFlux::Enum::none;
+  static const PrimalDiffusionFlux::Enum    diffFluxEnum   = PrimalDiffusionFlux::Enum::general;
+
+  //produce some static compiler warnings in case we are using an uninstalled solver
+  static const AvailableSolvers< solverEnum > checkSolverInstalled;
+
+  template< class GridImp >
+  struct StokesProblemCreator
+  {
+
+    struct SubStokesProblemCreator
+    {
+      typedef AlgorithmConfigurator< GridImp, galerkinEnum, adaptivityEnum, dfSpaceEnum, solverEnum, advLimiterEnum,
+                                     matrixEnum, advFluxEnum, diffFluxEnum > AC;
+
+      struct SubPoissonProblemCreator
+      {
+
+        typedef typename AC::GridType                         GridType;
+        typedef typename AC::GridParts                        HostGridPartType;
+        typedef HostGridPartType                              GridPartType;
+
+        // define problem type here if interface should be avoided
+        typedef typename Stokes::ProblemInterface< GridType >::PoissonProblemType  ProblemInterfaceType;
+
+        typedef typename ProblemInterfaceType::FunctionSpaceType      FunctionSpaceType;
+
+
+        struct AnalyticalTraits
+        {
+          typedef ProblemInterfaceType                                  ProblemType;
+          typedef ProblemInterfaceType                                  InitialDataType;
+          typedef Stokes::PoissonModel< GridPartType, InitialDataType > ModelType;
+
+          template< class Solution, class Model, class ExactFunction, class SigmaFunction>
+          static void addEOCErrors ( Solution &u, Model &model, ExactFunction &f, SigmaFunction& sigma )
+          {
+            static L2EOCError l2EocError( "$L^2$-Error" );
+            l2EocError.add( u, f );
+            static DGEOCError dgEocError( "DG-Error" );
+            dgEocError.add( u, f );
+            static H1EOCError sigmaEocError( "sigma-norm" );
+            sigmaEocError.add( sigma, f );
+          }
+        };
+
+        static inline std::string moduleName() { return "";}
+
+        static ProblemInterfaceType* problem()
+        {
+          return new typename Stokes::Problem< GridType, Stokes::GeneralizedProblem >::PoissonProblemType();
+          //return new typename Stokes::GeneralizedProblem< GridType >::PoissonProblemType();
+        }
+
+
+        template< int polOrd >
+        struct DiscreteTraits
+        {
+          typedef typename AC::template DiscreteFunctionSpaces< GridPartType, polOrd, FunctionSpaceType>
+                                                                                     DFSpaceType;
+        public:
+          typedef typename AC::template DiscreteFunctions< DFSpaceType >             DiscreteFunctionType;
+
+          typedef std::tuple< DiscreteFunctionType*, DiscreteFunctionType* >         IOTupleType;
+
+          class Operator
+          {
+            typedef typename AC::template DefaultAssembTraits< DFSpaceType, DFSpaceType, polOrd, AnalyticalTraits >
+                                                                                     OpTraits;
+          public:
+            typedef typename AC::template Operators< OpTraits >                      AssemblerType;
+            typedef typename AssemblerType::MatrixType                               type;
+          };
+
+          struct Solver
+          {
+            typedef typename AC::template LinearSolvers< DFSpaceType, true> type;
+          };
+
+          typedef SubSolverMonitor< SolverMonitor >                                  SolverMonitorType;
+          typedef SubDiagnostics< Diagnostics >                                      DiagnosticsType;
+        };
+
+        template <int polOrd>
+        using Algorithm = SubEllipticAlgorithm< GridType, SubPoissonProblemCreator, polOrd >;
+      };
+
+      typedef typename AC::GridType                         GridType;
+      typedef typename AC::GridParts                        HostGridPartType;
+      typedef HostGridPartType                              GridPartType;
+
+      // define problem type here if interface should be avoided
+      typedef Stokes::ProblemInterface< GridType >             ProblemInterfaceType;
+
+      typedef typename ProblemInterfaceType::StokesProblemType::FunctionSpaceType FunctionSpaceType;
+
+      struct AnalyticalTraits
+      {
+        typedef ProblemInterfaceType                                      ProblemType;
+        typedef ProblemInterfaceType                                      InitialDataType;
+        typedef Stokes::StokesModel< GridPartType, InitialDataType >      ModelType;
+
+        template< class Solution, class Model, class ExactFunction >
+        static void addEOCErrors ( Solution &u, Model &model, ExactFunction &f )
+        {
+          static L2EOCError l2EocError( "$L^2$-p-Error" );
+          l2EocError.add( u, f );
+        }
+      };
+
+      static inline std::string moduleName() { return "";}
+
+      static ProblemInterfaceType* problem()
+      {
+        return new Stokes::Problem< GridType, Stokes::GeneralizedProblem > ();
+      }
+
+      template< int polOrd >
+      struct DiscreteTraits
+      {
+      private:
+        typedef typename SubPoissonProblemCreator::template DiscreteTraits< polOrd > PoissonDiscreteTraits;
+        typedef typename PoissonDiscreteTraits::DiscreteFunctionType                 VelDiscreteFunctionType;
+        typedef typename AC::template DiscreteFunctionSpaces< GridPartType, polOrd, FunctionSpaceType>
+                                                                                     DFSpaceType;
+      public:
+        typedef typename AC::template DiscreteFunctions< DFSpaceType >               DiscreteFunctionType;
+
+        typedef std::tuple< DiscreteFunctionType*, DiscreteFunctionType* >           IOTupleType;
+        typedef std::tuple<>                                                         ExtraParameterTuple;
+
+        class Operator
+        {
+          typedef typename AC::template DefaultAssembTraits< DFSpaceType, DFSpaceType, polOrd, AnalyticalTraits >
+                                                                                     OpTraits;
+
+          typedef AssemblerTraitsList< std::tuple< VelDiscreteFunctionType, DiscreteFunctionType >, AC::template Containers > AssTraits;
+        public:
+          typedef StokesAssembler< AssTraits, OpTraits >                             AssemblerType;
+          //the following typedef is not needed by stokes algorithm atm
+          //typedef typename AssemblerType::MatrixType                               type;
+        };
+
+        struct Solver
+        {
+          typedef UzawaSolver< typename Operator::AssemblerType, typename PoissonDiscreteTraits::Solver::type >
+                                                                                     type;
+        };
+
+        static_assert( (int)DFSpaceType::FunctionSpaceType::dimRange == 1 , "pressure dimrange does not fit");
+
+        typedef SubSolverMonitor< SolverMonitor >                                    SolverMonitorType;
+        typedef SubDiagnostics< Diagnostics >                                        DiagnosticsType;
+      };
+
+      template <int polOrd>
+      using Algorithm = SubStokesAlgorithm< GridType, SubStokesProblemCreator, SubPoissonProblemCreator, polOrd >;
+    };
+
+
+    template <int polOrd>
+    using Algorithm = SteadyStateAlgorithm< polOrd, UncoupledSubAlgorithms, SubStokesProblemCreator >;
+
+    typedef GridImp                                         GridType;
+
+    static inline std::string moduleName() { return ""; }
+
+    static inline GridPtr<GridType>
+    initializeGrid() { return PoissonGridInitializer< GridType >::initializeGrid(); }
+
+
+  };
+}
+}
+#endif // FEMHOWTO_HEATSTEPPER_HH
---
+++ dune/fem-dg/examples/stokes/stokesalgorithm.hh
@@ -0,0 +1,433 @@
+#ifndef STOKES_ALGORITHM_HH
+#define STOKES_ALGORITHM_HH
+
+#ifndef NDEBUG
+// enable fvector and fmatrix checking
+#define DUNE_ISTL_WITH_CHECKING
+#endif
+
+// include std libs
+#include <iostream>
+#include <string>
+
+// dune-fem includes
+#include <dune/fem/misc/l2norm.hh>
+#include <dune/fem/operator/projection/l2projection.hh>
+#include <dune/fem/operator/lagrangeinterpolation.hh>
+#include <dune/fem/gridpart/common/gridpart.hh>
+#include <dune/fem/function/common/localfunctionadapter.hh>
+#include <dune/fem/operator/common/stencil.hh>
+
+// dune-fem-dg includes
+#include <dune/fem-dg/operator/adaptation/stokesestimator.hh>
+#include <dune/fem-dg/algorithm/sub/elliptic.hh>
+#include <dune/fem-dg/solver/uzawa.hh>
+#include <dune/fem-dg/misc/tupleutility.hh>
+
+// include local header files
+#include <dune/fem-dg/examples/stokes/stokesassembler.hh>
+
+namespace Dune
+{
+
+namespace Fem
+{
+
+  template< class GridPartImp, class DiscreteVelocityFunctionImp, class DiscretePressureFunctionImp, class SigmaFunctionSpaceImp, class AssemblerImp, class ModelImp, int polOrder>
+  class StokesSigmaEstimator: public PoissonSigmaEstimator<GridPartImp, DiscreteVelocityFunctionImp, SigmaFunctionSpaceImp, AssemblerImp, polOrder >
+  {
+    typedef PoissonSigmaEstimator<GridPartImp, DiscreteVelocityFunctionImp, SigmaFunctionSpaceImp, AssemblerImp, polOrder > BaseType;
+
+  public:
+    typedef DiscreteVelocityFunctionImp     DiscreteVelocityFunctionType;
+    typedef DiscretePressureFunctionImp     DiscretePressureFunctionType;
+    typedef SigmaFunctionSpaceImp           SigmaFunctionSpaceType;
+    typedef GridPartImp                     GridPartType;
+    typedef typename GridPartType::GridType GridType;
+    typedef AssemblerImp                    AssemblerType;
+    typedef ModelImp                        ModelType;
+
+    using BaseType::sigmaSpace_;
+    using BaseType::sigmaDiscreteFunction_;
+    using BaseType::sigmaLocalFunction_;
+    using BaseType::sigmaLocalEstimate_;
+    using BaseType::assembler_;
+    using BaseType::gridPart_;
+    using BaseType::solution_;
+
+    template<class DiscreteFunction,class DiscretePressureFunction,class Operator>
+    class SigmaEval: public BaseType::SigmaLocalType
+    {
+      typedef typename BaseType::SigmaLocalType SigmaBaseType;
+    public:
+      typedef DiscretePressureFunction DiscretePressureFunctionType;
+      typedef typename DiscretePressureFunctionType::RangeType PRangeType;
+
+    private:
+      const DiscretePressureFunctionType& ph_;
+      typename DiscretePressureFunctionType::LocalFunctionType localp_;
+
+    public:
+      SigmaEval(const DiscreteFunction &uh,
+          const DiscretePressureFunctionType &ph,
+          const Operator& oper)
+        : SigmaBaseType(uh,oper),
+         ph_(ph),
+         localp_(ph_)
+      {}
+      SigmaEval(const SigmaEval &other)
+      : SigmaBaseType(other), ph_(other.ph_), localp_(ph_)
+      {}
+      void init(const typename SigmaBaseType::EntityType &en)
+      {
+        SigmaBaseType::init(en);
+        localp_.init(en);
+      }
+      template< class PointType >
+      void evaluate(const PointType& x,typename SigmaBaseType::RangeType& res) const
+      {
+        SigmaBaseType::evaluate(x,res);
+        PRangeType p;
+        localp_.evaluate(x,p);
+        Dune::Fem::FieldMatrixConverter< typename SigmaBaseType::RangeType, typename DiscreteFunction::JacobianRangeType> res1( res );
+        for(int i=0;i<res1.rows;++i)
+          res1[i][i] -= p;
+      }
+    };
+
+    struct StokesFlux
+    {
+      typedef typename GridPartType::GridType::template Codim<0>::Entity EntityType;
+      typedef typename GridPartType::IntersectionIteratorType IntersectionIteratorType;
+      typedef typename GridPartType::IntersectionType IntersectionType;
+
+      typedef typename AssemblerType::FaceQuadratureType   FaceQuadratureType;
+      typedef typename AssemblerType::VolumeQuadratureType VolumeQuadratureType;
+
+      StokesFlux(const DiscretePressureFunctionType &p, const AssemblerType &oper)
+      : p_(p),
+        oper_(oper)
+        {}
+      template <class Quadrature,class Value,class DValue,class RetType, class DRetType>
+      void flux(const GridPartType &gridPart,
+                const IntersectionType &intersection,
+                const EntityType &entity, const EntityType &neighbor,
+                const double time,
+                const Quadrature &faceQuadInside, const Quadrature &faceQuadOutside,
+                const Value &valueEn, const DValue &dvalueEn,
+                const Value &valueNb, const DValue &dvalueNb,
+                RetType &retEn, DRetType &dretEn,
+                RetType &retNb, DRetType &dretNb) const
+      {
+        //\hat{K}
+        oper_.flux(gridPart,intersection,entity,neighbor,time,faceQuadInside,faceQuadOutside,
+                   valueEn,dvalueEn,valueNb,dvalueNb,retEn,dretEn,retNb,dretNb);
+        typename DiscretePressureFunctionType::LocalFunctionType pEn = p_.localFunction(entity);
+        typename DiscretePressureFunctionType::LocalFunctionType pNb = p_.localFunction(neighbor);
+        std::vector< typename DiscretePressureFunctionType::RangeType > pValuesEn( faceQuadInside.nop() );
+        std::vector< typename DiscretePressureFunctionType::RangeType > pValuesNb( faceQuadOutside.nop() );
+
+        pEn.evaluateQuadrature( faceQuadInside, pValuesEn );
+        pNb.evaluateQuadrature( faceQuadOutside, pValuesNb );
+
+        assert(retEn.size() == faceQuadInside.nop() );
+
+        for (unsigned int i=0;i<retEn.size();++i)
+        {
+          typename IntersectionType::GlobalCoordinate normal = intersection.integrationOuterNormal( faceQuadInside.localPoint(i) );
+          double value=0.5*(pValuesEn[i]+pValuesNb[i]);
+          normal*=value;
+
+          retEn[i]+=normal;
+          retNb[i]+=normal;
+
+        }
+
+      }
+
+      template<class Quadrature,class RetType>
+      void boundaryValues(const GridPartType &gridPart,
+                          const IntersectionType &intersection,
+                          const EntityType &entity,
+                          const double time,
+                          const Quadrature &faceQuadInside,
+                          RetType &retEn) const
+      {
+        oper_.boundaryValues(gridPart,
+                             intersection, entity, time, faceQuadInside,
+                             retEn);
+      }
+
+
+      template<class Quadrature,class Value,class DValue,class RetType, class DRetType>
+      void boundaryFlux(const GridPartType &gridPart,
+                const IntersectionType &intersection,
+                const EntityType &entity,
+                const double time,
+                const Quadrature &faceQuadInside,
+                const Value &valueEn, const DValue &dvalueEn,
+                const Value &valueNb,
+                RetType &retEn, DRetType &dretEn) const
+
+      {
+        oper_.boundaryFlux(gridPart,
+                           intersection, entity, time, faceQuadInside,
+                           valueEn, dvalueEn, valueNb,
+                           retEn, dretEn );
+        typename DiscretePressureFunctionType::LocalFunctionType pEn = p_.localFunction(entity);
+        std::vector< typename DiscretePressureFunctionType::RangeType > pValuesEn( faceQuadInside.nop() );
+
+        pEn.evaluateQuadrature( faceQuadInside, pValuesEn );
+        assert(retEn.size() == faceQuadInside.nop() );
+
+        for (unsigned int i=0;i<retEn.size();++i)
+        {
+          typename IntersectionType::GlobalCoordinate normal = intersection.integrationOuterNormal( faceQuadInside.localPoint(i) );
+          normal*=pValuesEn[i];
+
+          retEn[i]+=normal;
+
+        }
+      }
+      const ModelType &model() const
+      {
+        return oper_.model();
+      }
+      private:
+      const DiscretePressureFunctionType &p_;
+      const AssemblerType &oper_;
+    };
+
+
+
+    typedef Dune::Fem::LocalFunctionAdapter< SigmaEval<DiscreteVelocityFunctionType,DiscretePressureFunctionType,AssemblerType> > StokesEstimateFunction;
+    typedef StokesErrorEstimator< DiscreteVelocityFunctionType, StokesEstimateFunction, StokesFlux > StokesEstimatorType;
+    typedef Dune::Fem::LocalFunctionAdapter< StokesEstimatorType >          StokesEstimateDataType;
+
+
+    StokesSigmaEstimator( GridPartType& gridPart,
+                          const DiscreteVelocityFunctionType& solution,
+                          const DiscretePressureFunctionType& pressureSolution,
+                          const AssemblerType& assembler,
+                          const std::string keyPrefix = "" )
+    : BaseType( gridPart, solution, assembler, keyPrefix ),
+      stkFlux_( pressureSolution, assembler_),
+      stkLocalEstimate_( solution_, pressureSolution, assembler_ ),
+      stkEstimateFunction_("stokes estimate", stkLocalEstimate_, gridPart_, solution_.space().order() ),
+      stkEstimator_( solution_, stkEstimateFunction_, stkFlux_, gridPart_.grid() ),
+      stkEstimateData_("stokesEstimator", stkEstimator_, gridPart_, solution_.space().order() )
+    {}
+
+
+    StokesEstimatorType& estimator() const
+    {
+      return stkEstimator_;
+    };
+
+    StokesEstimateDataType& data() const
+    {
+      return stkEstimateData_;
+    }
+
+
+  private:
+    StokesFlux stkFlux_;
+    SigmaEval<DiscreteVelocityFunctionType,DiscretePressureFunctionType,AssemblerType> stkLocalEstimate_;
+
+    StokesEstimateFunction stkEstimateFunction_;
+    StokesEstimatorType  stkEstimator_;
+    StokesEstimateDataType  stkEstimateData_;
+
+
+  };
+
+  /**
+   *  \brief Algorithm for solving the Stokes equation.
+   *
+   *  \ingroup SubAlgorithms
+   */
+  template <class GridImp, class ProblemTraits, class ElliptProblemTraits, int polOrd >
+  class SubStokesAlgorithm : public SubSteadyStateAlgorithm<GridImp,ProblemTraits,polOrd>
+  {
+
+    typedef SubSteadyStateAlgorithm<GridImp,ProblemTraits,polOrd> BaseType;
+
+  public:
+    typedef typename ElliptProblemTraits::template Algorithm<polOrd> EllipticalAlgorithmType;
+
+    // type of Grid
+    typedef typename BaseType::GridType                             GridType;
+
+    // Choose a suitable GridView
+    typedef typename BaseType::GridPartType                         GridPartType;
+
+    // initial data type
+    typedef typename BaseType::ProblemType                          ProblemType;
+
+    // An analytical version of our model
+    typedef typename BaseType::ModelType                            ModelType;
+
+    typedef typename BaseType::OperatorType::AssemblerType          AssemblerType;
+
+    typedef typename EllipticalAlgorithmType::DiscreteTraits::DiscreteFunctionType
+                                                                    DiscreteVelocityFunctionType;
+    typedef typename BaseType::DiscreteFunctionType                 DiscreteFunctionType;
+    // ... as well as the Space type
+    typedef typename EllipticalAlgorithmType::DiscreteTraits::DiscreteFunctionType::DiscreteFunctionSpaceType
+                                                                    DiscreteVelocityFunctionSpaceType;
+    typedef typename BaseType::DiscreteFunctionSpaceType            DiscreteFunctionSpaceType;
+
+    typedef typename BaseType::SolverMonitorType                    SolverMonitorType;
+
+    // type of inverse operator (i.e. linear solver implementation)
+    typedef typename BaseType::SolverType::type                     SolverType;
+
+    enum { dimension = GridType::dimension  };
+
+    typedef typename DiscreteVelocityFunctionSpaceType ::
+      template ToNewDimRange< dimension * ElliptProblemTraits::AnalyticalTraits::ModelType::dimRange >::NewFunctionSpaceType
+                                                                    SigmaFunctionSpaceType;
+
+    typedef StokesSigmaEstimator< GridPartType, DiscreteVelocityFunctionType, DiscreteFunctionType,
+                                  SigmaFunctionSpaceType, typename EllipticalAlgorithmType::AssemblerType,
+                                  typename ElliptProblemTraits::AnalyticalTraits::ModelType, polOrd >
+                                                                    StokesSigmaEstimatorType;
+
+    typedef CovariantTuple< typename BaseType::IOTupleType::type, typename EllipticalAlgorithmType::IOTupleType::type >
+                                                                    IOTupleType;
+
+    typedef typename BaseType::TimeProviderType                     TimeProviderType;
+
+    typedef typename BaseType::AnalyticalTraits                     AnalyticalTraits;
+
+    using BaseType::problem;
+    using BaseType::model;
+    using BaseType::name;
+    using BaseType::grid;
+    using BaseType::rhs;
+    using BaseType::rhs_;
+    using BaseType::gridWidth;
+    using BaseType::gridSize;
+    using BaseType::solution;
+    using BaseType::solver_;
+    using BaseType::exactSolution_;
+
+    typedef typename EllipticalAlgorithmType::ContainerType         EllipticContainerType;
+
+    typedef typename AssemblerType::ContainerType                   ContainerType;
+
+  public:
+
+    explicit SubStokesAlgorithm( GridType& grid, ContainerType& container ) :
+      BaseType( grid, container.template adapter<1>() ),
+      container_( container ),
+      space_( container_.template adapter<1>().space() ),
+      ellAlg_( grid, container_.template adapter<0>() ),
+      assembler_( container_, model() ),
+      ioTuple_( new IOTupleType( *BaseType::dataTuple(), *ellAlg_.dataTuple() ) ),
+      stokesSigmaEstimator_( new StokesSigmaEstimatorType( container_.template adapter<1>().gridPart(), ellAlg_.solution(), solution(), ellAlg_.assembler(), name() ) )
+    {
+    }
+
+    virtual IOTupleType& dataTuple ()
+    {
+      return *ioTuple_;
+    }
+
+    bool adaptation(const double tolerance)
+    {
+#if 0
+#if PADAPTSPACE
+      // update to current
+      polOrderContainer_.resize();
+
+      const double error = stokesSigmaEstimator_.estimator().estimate( problem() );
+      typedef typename DiscreteVelocityFunctionSpaceType::IteratorType IteratorType;
+      const IteratorType end = ellAlg_.space().end();
+      for( IteratorType it = ellAlg_.space().begin(); it != end; ++it )
+      {
+        const typename IteratorType::Entity &entity = *it;
+        polOrderContainer_[entity].value() =
+          stokesSigmaEstimator_.estimator().newOrder( 0.98*tolerance, entity );
+      }
+#endif
+#endif
+      return (error < std::abs(tolerance) ? false : stokesSigmaEstimator_->estimator().mark( 0.98 * tolerance));
+    }
+
+  private:
+    virtual std::shared_ptr< SolverType > doCreateSolver() override
+    {
+      Dune::Timer timer;
+      timer.start();
+
+      //if( rhsOperator_ ) //rhs by external rhs operator
+      //  rhsOperator_( solution(), rhs() );
+      rhs().clear();
+      assembler_.assemble();
+
+      std::cout << "Solver (Stokes) assemble time: " << timer.elapsed() << std::endl;
+
+      double absLimit = Dune::Fem::Parameter::getValue<double>("istl.absLimit",1.e-10);
+#if 0
+#ifdef PADAPTSPACE
+      int polOrder = Dune::Fem::Parameter::getValue<double>("femdg.polOrd",1);
+      // only implemented for PAdaptiveSpace
+      std::vector<int> polOrderVec( ellAlg_.space().gridPart().indexSet().size(0) );
+      std::vector<int> polOrderVecPressure( space_.gridPart().indexSet().size(0) );
+      std::fill( polOrderVec.begin(), polOrderVec.end(), polOrder );
+      std::fill( polOrderVecPressure.begin(), polOrderVecPressure.end(), polOrder-1 );
+      ellAlg_.space().adapt( polOrderVec );
+      space_.adapt( polOrderVecPressure);
+#endif
+#endif
+      return std::make_shared< SolverType >( container_, *ellAlg_.solver(), absLimit, 3*ellAlg_.solution().space().size() );
+    }
+
+    virtual void doInitialize ( const int loop ) override
+    {
+      ellAlg_.initialize( loop );
+      BaseType::doInitialize( loop );
+    }
+
+    virtual void doPreSolve( const int loop ) override
+    {
+      ellAlg_.preSolve( loop );
+      BaseType::doPreSolve( loop );
+    }
+
+    virtual void doSolve( const int loop ) override
+    {
+      BaseType::doSolve( loop );
+
+      // TODO check wheather we need the following line
+      stokesSigmaEstimator_->update();
+    }
+
+    virtual void doPostSolve( const int loop ) override
+    {
+      ellAlg_.postSolve( loop );
+      BaseType::doPostSolve( loop );
+    }
+
+    //! finalize computation by calculating errors and EOCs
+    virtual void doFinalize( const int loop ) override
+    {
+      ellAlg_.finalize( loop );
+      AnalyticalTraits::addEOCErrors( solution(), ellAlg_.model(), problem().get<1>().exactSolution() );
+    }
+
+  protected:
+    ContainerType&                              container_;
+    const DiscreteFunctionSpaceType&            space_;
+    AssemblerType                               assembler_;
+
+    EllipticalAlgorithmType                     ellAlg_;
+    std::unique_ptr< IOTupleType >              ioTuple_;
+    std::unique_ptr< StokesSigmaEstimatorType > stokesSigmaEstimator_;
+  };
+
+
+}
+}
+#endif // FEMHOWTO_STEPPER_HH
