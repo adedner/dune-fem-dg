32556864c4e57b219268c6c45603fc2ebb7589ed
Stefan Girke <stefan.girke@wwu.de>
2016-02-11 11:06:34 +0100

    add FEMDG_FAST_TESTBUILD CMake option to build only important tests, default is on

--- cmake/modules/DuneFemDgMacros.cmake
+++ cmake/modules/DuneFemDgMacros.cmake
@@ -6,5 +6,11 @@ if(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_BINARY_DIR})
   set(GENERATE_FEMDG_PARAMETER_FILE FALSE)
 endif()
 
+# do a fast test build by default, 
+# i.e. only build the most important tests 
+# when calling 'make test' and 'make build_tests', respectively
+set(FEMDG_FAST_TESTBUILD ON CACHE BOOL 
+    "only build the most important tests when calling 'make test'" )
+
 include(Codegen)
 include(TargetDistclean)
