c60c2460e48665eef3373690c248d50ef57d3f64
Tobias Malkmus <tomalk@mathematik.uni-freiburg.de>
2016-02-11 15:12:50 +0100

    [scripts] adapt check scripts to cmake based builds

--- scripts/check-tests.sh
+++ scripts/check-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if test $# -lt 1 ; then
-  echo "Usage: $0 <dune-fem-dir> [MAKE_CHECK_FLAGS]"
+  echo "Usage: $0 <dune-base-dir> [MAKE_CHECK_FLAGS]"
   exit 1
 fi
 
@@ -9,25 +9,39 @@ MAKE_CHECK_FLAGS=$2
 
 WORKINGDIR=`pwd`
 cd $1
-FEMDIR=`pwd`
+BUILDDIR=`pwd`
 
-CHECKLOG=$WORKINGDIR/check-tests.out
-make build_tests $MAKE_CHECK_FLAGS -i && make -i test $MAKE_CHECK_FLAGS  &> $CHECKLOG
+echo
+echo "Performing make build_tests && make test in $(basename $BUILDDIR) ..."
+
+CHECKLOG=$WORKINGDIR/check-tests.log
+make -i build_tests $MAKE_CHECK_FLAGS  &> $CHECKLOG
+make test &> /dev/null
 
 retvalue=0
 warnings=`grep warning: $CHECKLOG | grep -v "default CommunicationManager is used" | grep -v "GRIDDIM not defined" | grep -v "No GRIDTYPE defined" | grep -v "Hdiv-Projection only working for polOrd = 1" | grep -v "YaspGrid does not provide a HierarchicIndexSet" | wc -l`
 if test $warnings -gt 0 ; then
   echo "Warning: $warnings compiler warnings occurred."
 fi
-errors=`grep Error $CHECKLOG | wc -l`
+
+errors=`grep error: $CHECKLOG | wc -l`
 if test $errors -gt 0 ; then
   echo "Error: $errors compile time errors occurred."
   retvalue=1
 fi
+
 urefs=`grep ": undefined reference" $CHECKLOG | wc -l`
 if test $urefs -gt 0 ; then
   echo "Error: $urefs undefined linker references occurred."
   retvalue=1
 fi
+
+# if a test has faild this file is writen
+if test -e $BUILDDIR/Testing/Temporary/LastTestsFailed.log ; then
+  retvalue=1
+fi
+
+# copy files with test logs and timings
+mv $BUILDDIR/Testing/Temporary/* $WORKINGDIR/
 
 exit $retvalue
--- scripts/fullcheck.sh
+++ scripts/fullcheck.sh
@@ -10,9 +10,7 @@ if test \( $# -lt 1 \) -o ! -e $1/$DUNECONTROL ; then
   exit 1
 fi
 
-MODNAME="dune-fem-dg"
-
-echo "Full Check of $MODNAME"
+echo "Full Check of dune-fem-dg"
 echo "-------------------------"
 
 echo
@@ -25,95 +23,28 @@ echo "Host Type: $HOSTTYPE"
 WORKINGDIR=`pwd`
 cd $1
 DUNEDIR=`pwd`
-FEMDIR="$DUNEDIR/$MODNAME"
-BUILDDIR="$DUNEDIR"
+FEMDIR="$DUNEDIR/dune-fem-dg"
 SCRIPTSDIR="$FEMDIR/scripts"
 OPTSDIR="$SCRIPTSDIR/opts"
 
-# search for all existing dune modules
-# ------------------------------------
-MODULES=""
-for modctrl in $(find -name dune.module -print); do
-  MODULES+="$(dirname $modctrl | sed -e 's@^[.]/@@g') " 
-done
-
 errors=0
 
-# configure with minimal options
-# ------------------------------
-
-MINIMALOPTS="$OPTSDIR/minimal.opts"
-BUILDDIR="$BUILDDIR/$(source $MINIMALOPTS; echo $BUILD_DIR)"
-
-MAKE_FLAGS=""
-MAKE_FLAGS="$(source $MINIMALOPTS; echo $MAKE_FLAGS)"
-
-if test ! -e $MINIMALOPTS ; then
-  echo "Error: $MINIMALOPTS not found."
+NONOPTSFILE=""
+if test -e $OPTSDIR/config_none.opts; then
+  NONOPTSFILE="config_none.opts"
+else
+  echo "config_none.opts not found."
   exit 1
 fi
 
+echo ""
+echo "Performing checks for config_none.opts..."
 
-minimal_configure()
-{
-  local check=`mktemp -p $WORKINGDIR check.XXXXXX`
-  {
-    $DUNECONTROL --opts=$MINIMALOPTS --builddir=$BUILDDIR all
-    echo $? > $check
-  } 2>&1 | dd conv=notrunc > $WORKINGDIR/minimal-conf.out 2>/dev/null
-  local return_value=`cat $check`
-  rm $check
-  return $return_value
-}
-
-
-echo
-echo "Configuring with minimal options..."
-cd $DUNEDIR
-if ! minimal_configure ; then
-  echo "Fatal: Cannot configure with minimal options (see $WORKINGDIR/minimal-conf.out)."
-  exit 1
-fi
-
-
-# check headers
-# -------------
-
-for module in $MODULES;
-do
-  CHECKLOG="$WORKINGDIR/minimal-hc-$module.out"
-  echo
-  echo "Checking headers in $module ..."
-  cd $BUILDDIR/$module
-  make headercheck $MAKE_FLAGS -i &> $CHECKLOG
-  hc_errors=$(grep error: $CHECKLOG)
-  if test -z "$hc_errors" ; then
-    rm $CHECKLOG
-  else
-    if test "x$module" == "x$MODNAME"; then
-      echo "Error: headercheck for module $module failed (see $CHECKLOG )"
-      errors=$((errors+1))
-    else
-      echo "Warning: headercheck for module $module failed (see $CHECKLOG )"
-    fi
-  fi
-done
-
-
-# perform make test
-# ------------------
-
-cd $WORKINGDIR 
-echo
-echo "Checking for minimal options ..."
-
-CHECKLOG="$WORKINGDIR/minimal-check.out"
-
-if ! $SCRIPTSDIR/check-tests.sh $BUILDDIR/$MODNAME "$MAKE_FLAGS"; then
-  echo "Error: make test failed with minimal options (see $CHECKLOG)"
+cd $WORKINGDIR
+# perform headercheck only ones
+if ! $SCRIPTSDIR/check-opts.sh $DUNEDIR $NONOPTSFILE true; then
   errors=$((errors+1))
 fi
-mv $WORKINGDIR/check-tests.out $CHECKLOG
 
 if test $errors -gt 0 ; then
   exit 1
