73ba4bf40be27a4690d1c6cf46559cf8ab9fc826
Robert K <robertk@posteo.org>
2016-03-02 15:31:53 +0100

    Revised build script.

--- scripts/build-dune-fem-dg.sh
+++ scripts/build-dune-fem-dg.sh
@@ -18,58 +18,44 @@ fi
 # to build and run the examples in dune-fem-dg
 # NOTE: Zoltan has to be downloaded separately from
 
-#change appropriately, i.e. 2.3 or empty (which refers to master)
+#change appropriately, i.e. 2.4 or empty (which refers to master)
 DUNEVERSION=2.4
 
 # your favorite compiler optimization flags
 FLAGS="-O3 -DNDEBUG"
 MAKE_FLAGS="-j4"
 
-# download dlmalloc from ftp://g.oswego.edu/pub/misc/ via the following command
-if ! test -d dlmalloc ; then
-  echo "Downloading dlmalloc"
-  mkdir dlmalloc ; cd dlmalloc
-  wget ftp://g.oswego.edu/pub/misc/malloc.{h,c}
-  cd ../
-fi
-# configure parameter for dlmalloc (v 2.8.6)
-WITH_DLMALLOC="--with-dlmalloc=$WORKDIR/dlmalloc"
-
-# most likely /usr if zlib is installed on the system
-#WITH_ZLIB="--with-zlib=/usr"
-WITH_ZLIB=
+# PETSc from http://www.mcs.anl.gov/petsc/
+# set the environment variable PETSC_DIR for PETSc to be found
+# export PETSC_DIR=$WORKDIR/petsc
 
 # Zoltan has to be downloaded from
 # http://www.cs.sandia.gov/zoltan/
-#WITH_ZOLTAN="--with-zoltan=$WORKDIR/zoltan"
+#WITH_ZOLTAN="-DZOLTAN_ROOT=$WORKDIR/zoltan"
 WITH_ZOLTAN=
 
-# SIONlib 1.5p1 has to be downloaded from
+# SIONlib 1.5p1 (or later) has to be downloaded from
 # http://www.fz-juelich.de/ias/jsc/EN/Expertise/Support/Software/SIONlib/sionlib-download_node
-#WITH_SIONLIB="--with-sionlib=$WORKDIR/sionlib"
+#WITH_SIONLIB="-DSIONLIB_ROOT=$WORKDIR/sionlib"
 WITH_SIONLIB=
 
 # dune modules needed to build dune-fem-dg
 DUNEMODULES="dune-common dune-geometry dune-grid dune-istl dune-alugrid dune-fem dune-fem-dg"
 
+# build dir for cmake (. for in-source build or empty then this will default to build-cmake)
+BUILDDIR=.
+
 # build flags for all DUNE modules
 # change according to your needs
-CACHEFILE=$WORKDIR/cache.config
-# if ! test -f config.opts ; then
-echo "MAKE_FLAGS=\"$MAKE_FLAGS\"
-USE_CMAKE=yes
-CONFIGURE_FLAGS=\"CXXFLAGS=\\\"$FLAGS\\\" \\
-  --cache-file=$CACHEFILE \\
-  --disable-documentation \\
-  --Wno-deprecated-declarations \\
-  --enable-experimental-grid-extensions \\
-  --enable-parallel \\
-  --enable-fieldvector-size-is-method \\
-  $WITH_DLMALLOC \\
-  $WITH_ZLIB \\
-  $WITH_ZOLTAN \\
-  $WITH_SIONLIB\"" > config.opts
-# fi
+if ! test -f config.opts ; then
+  echo "MAKE_FLAGS=\"$MAKE_FLAGS\" \\
+  USE_CMAKE=yes \\
+  BUILDDIR=$BUILDDIR \\
+  CMAKE_FLAGS=\"-DCMAKE_CXX_FLAGS=\\\"$FLAGS\\\" \\
+   -DDUNE_GRID_EXPERIMENTAL_GRID_EXTENSIONS=TRUE \\
+   -DCMAKE_DISABLE_FIND_PACKAGE_LATEX=TRUE \\
+   $WITH_ZOLTAN\" " > config.opts
+fi
 
 DUNEBRANCH=
 ALUGRIDBRANCH=
@@ -98,19 +84,15 @@ for MOD in $DUNEMODULES ; do
   fi
 done
 
-# delete old cache file
-if test -f $CACHEFILE ; then
-  rm -f $CACHEFILE
-fi
-
 # build all DUNE modules in the correct order
 ./dune-common/bin/dunecontrol --opts=config.opts all
 
-cd dune-fem-dg/build-cmake
+if [ "$BUILDDIR" == "" ]; then
+  cd dune-fem-dg/build-cmake
+else
+  cd dune-fem-dg/$BUILDDIR/
+fi
 TARGET=test
-BUILD_TARGET=build_tests
 
-# first build tests (compiling...)...
-make $MAKE_FLAGS $BUILD_TARGET
-# ... then run tests
+# compile and run tests
 make $MAKE_FLAGS $TARGET
