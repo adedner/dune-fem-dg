16e799536caf64127626cf42e22b1e236ca84cc7
Robert K <robertk@posteo.org>
2016-03-02 21:35:30 +0100

    [bugfix] enable EOC calculation for Poisson again.

--- dune/fem-dg/examples/poisson/algorithmcreator.hh
+++ dune/fem-dg/examples/poisson/algorithmcreator.hh
@@ -55,7 +55,7 @@ namespace Fem
                                      Galerkin::Enum::dg,
                                      Adaptivity::Enum::yes,
                                      DiscreteFunctionSpaces::Enum::legendre,
-                                     Solver::Enum::femoem,
+                                     Solver::Enum::istl,
                                      AdvectionLimiter::Enum::unlimited,
                                      Matrix::Enum::assembled,
                                      AdvectionFlux::Enum::upwind,
