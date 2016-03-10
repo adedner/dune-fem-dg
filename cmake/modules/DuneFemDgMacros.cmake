99ca3963f98c427a0dfdbc47340af15c0fc53cd9
Stefan Girke <stefan.girke@wwu.de>
2016-03-10 18:16:58 +0100

    added a new, more advanced testing scheme
    - allow to use an executable with different parameter files ("test case")
    - avoid overwriting of existing data folders by different test cases

--- cmake/modules/DuneFemDgMacros.cmake
+++ cmake/modules/DuneFemDgMacros.cmake
@@ -7,13 +7,32 @@ if(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_BINARY_DIR})
 endif()
 
 # macro for configuring the parameter files from parameter.in
-macro(configure_parameter_file)
+function(configure_parameter_file)
   set(CURRENT_PARAMETER_PATH .)
   if(OUT_OF_SOURCE_FEMDG_PARAMETER_FILE)
     set(CURRENT_PARAMETER_PATH ${CMAKE_CURRENT_SOURCE_DIR})
   endif()
-  configure_file(parameter.in ${CMAKE_CURRENT_BINARY_DIR}/parameter)
-endmacro(configure_parameter_file)
+  set( TESTCASE_OUTPUT "data" )
+  set( TESTCASE_INPUT "parameter" )
+  if( ARGC EQUAL 1 )
+    set( TESTCASE_OUTPUT "data/${ARGV0}" )
+    set( TESTCASE_INPUT "parameters/${ARGV0}" )
+  endif()
+  if( ARGC EQUAL 2 )
+    set( TESTCASE_OUTPUT "data/${ARGV0}" )
+    set( TESTCASE_INPUT "${ARGV1}" )
+  endif()
+  configure_file(${CMAKE_SOURCE_DIR}/cmake/scripts/parameter.in ${CMAKE_CURRENT_BINARY_DIR}/${TESTCASE_INPUT} )
+endfunction(configure_parameter_file)
+
+function(dune_add_test_case base_target)
+  foreach( testcase ${ARGN})
+    set( TESTCASE_PARAMFILE parameters/${base_target}_${testcase} )
+    configure_parameter_file( ${testcase} ${TESTCASE_PARAMFILE} )
+    add_test( NAME ${base_target}_${testcase} COMMAND ./${base_target} ${TESTCASE_PARAMFILE} )
+  endforeach()
+endfunction()
+
 
 # do a fast test build by default,
 # i.e. only build the most important tests
