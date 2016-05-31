dd1a0866d89656b948e0c4f35e855694d0afe130
Stefan Girke <stefan.girke@wwu.de>
2016-05-31 17:18:08 +0200

    move all parameters to own directory

--- cmake/modules/DuneFemDgMacros.cmake
+++ cmake/modules/DuneFemDgMacros.cmake
@@ -1,28 +1,28 @@
 # the main files for all the tests are in the same dir
 set(FEMDG_MAIN_DIR "${CMAKE_SOURCE_DIR}/dune/fem-dg/main")
 
-set(OUT_OF_SOURCE_FEMDG_PARAMETER_FILE TRUE)
-if(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_BINARY_DIR})
-  set(OUT_OF_SOURCE_FEMDG_PARAMETER_FILE FALSE)
-endif()
-
 # macro for configuring the parameter files from parameter.in
 function(configure_parameter_file)
-  set(CURRENT_PARAMETER_PATH .)
-  if(OUT_OF_SOURCE_FEMDG_PARAMETER_FILE)
-    set(CURRENT_PARAMETER_PATH ${CMAKE_CURRENT_SOURCE_DIR})
-  endif()
+  set(CURRENT_PARAMETER_PATH ${CMAKE_CURRENT_SOURCE_DIR})
+
+  #default value
   set( TESTCASE_OUTPUT "data" )
   set( TESTCASE_INPUT "parameter" )
+  
   if( ARGC EQUAL 1 )
-    set( TESTCASE_OUTPUT "data/${ARGV0}" )
-    set( TESTCASE_INPUT "parameters/${ARGV0}" )
+    set( TESTCASE_OUTPUT "${ARGV0}" )
+    set( TESTCASE_INPUT "${ARGV0}" )
   endif()
+ 
   if( ARGC EQUAL 2 )
-    set( TESTCASE_OUTPUT "data/${ARGV0}" )
+    set( TESTCASE_OUTPUT "${ARGV0}" )
     set( TESTCASE_INPUT "${ARGV1}" )
   endif()
-  configure_file(${CMAKE_SOURCE_DIR}/cmake/scripts/parameter.in ${CMAKE_CURRENT_BINARY_DIR}/${TESTCASE_INPUT} )
+
+  # write start parameter file
+  # This file points to the real parameter file contained in the directory "parameters"
+  configure_file(${CMAKE_SOURCE_DIR}/cmake/scripts/parameter.in ${CMAKE_CURRENT_BINARY_DIR}/parameter )
+
 endfunction(configure_parameter_file)
 
 function(dune_add_test_case base_target)
