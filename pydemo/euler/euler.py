7e533976e1f48ce6832c653bf8bebac44f51932a
dedner <a.s.dedner@warwick.ac.uk>
2018-07-27 11:42:13 +0100

    add a euler module

---
+++ pydemo/euler/euler.py
@@ -0,0 +1,9 @@
+from ufl import *
+exec(open("euler.ufl").read())
+
+def RiemanProblem(UL,UR):
+    x = SpatialCoordinate(space.cell())
+    return conditional(x[0]<0,as_vector(UL),as_vector(UR))
+def Sod():
+    return RiemanProblem( Model.toCons([1,0,0,1]),
+                          Model.toCons([0.125,0,0,0.1]) )
