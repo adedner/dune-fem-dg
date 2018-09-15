6502141c87971efd881bc72c9244b680ea6670e9
dedner <a.s.dedner@warwick.ac.uk>
2018-09-15 08:55:57 +0100

    removed some unused files

--- pydemo/euler/main.py
+++ pydemo/euler/main.py
@@ -27,7 +27,7 @@ parameters = {"fem.ode.odesolver": "EX",
 
 
 run(*problem(),
-        startLevel=0, polOrder=2, limiter="default",
+        startLevel=0, polOrder=0, limiter=None, # "default",
         primitive=primitive, saveStep=0.1, subsamp=2,
         dt=None,threading=True,grid="alucube",
         parameters=parameters)
