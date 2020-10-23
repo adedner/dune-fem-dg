acbfb16c1e156856675d43e5f342f0087e18c9bb
Robert K <robertk@posteo.org>
2020-10-23 18:10:50 +0200

    [cleanup][TroubledCellIndicator] added virtual destructor.

--- dune/fem-dg/operator/limiter/indicatorbase.hh
+++ dune/fem-dg/operator/limiter/indicatorbase.hh
@@ -6,6 +6,8 @@ namespace Dune
     template< class DiscreteFunction >
     struct TroubledCellIndicatorBase
     {
+      virtual ~TroubledCellIndicatorBase() {}
+
       typedef typename DiscreteFunction::LocalFunctionType LocalFunctionType;
       virtual double operator()( const DiscreteFunction& U, const LocalFunctionType& uEn ) const = 0;
     };
