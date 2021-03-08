888ad5f2a4d70b18bab0788c7aa86a2bb628564e
Robert K <robertk@posteo.org>
2021-03-08 16:23:30 +0100

    [bugfix][Python] make shock_tube_fast run again.

--- pydemo/euler/shock_tube_fast.py
+++ pydemo/euler/shock_tube_fast.py
@@ -1,3 +1,8 @@
+import os
+
+# set number of threads to be used for thread parallel version
+os.environ['OMP_NUM_THREADS'] = '4'
+
 import time
 from dune.grid import structuredGrid, cartesianDomain
 from dune.fem import parameter
@@ -13,7 +18,7 @@ dim = 2
 from euler import sod as problem
 #from euler import radialSod3 as problem
 
-Model, initial, domain, endTime, name, exact = problem(dim,gamma)
+Model = problem(dim,gamma)
 
 parameter.append({"fem.verboserank": -1})
 parameter.append({"fem.timeprovider.factor": 0.25})
@@ -25,7 +30,7 @@ parameters = {"fem.ode.odesolver": "EX",
               "femdg.limiter.admissiblefunctions": 1,
               "femdg.limiter.tolerance": 1e-8}
 
-x0,x1,N = domain
+x0,x1,N = Model.domain
 grid = structuredGrid(x0,x1,N)
 # grid = create.grid("ALUSimplex", cartesianDomain(x0,x1,N))
 dimR     = grid.dimension + 2
@@ -36,7 +41,7 @@ saveStep = 0.15
 saveTime = saveStep
 
 def initialize(space):
-    return space.interpolate(initial, name='u_h')
+    return space.interpolate(Model.initial, name='u_h')
     if space.order == 0:
         return space.interpolate(initial, name='u_h')
     else:
@@ -60,7 +65,7 @@ def useODESolver(polOrder=2, limiter='default'):
 
     operator.applyLimiter( u_h )
     print("number of elements: ",grid.size(0),flush=True)
-    grid.writeVTK(name,
+    grid.writeVTK(Model.name,
         pointdata=[u_h],
         # celldata={"density":rho, "pressure":p}, # bug: density not shown correctly
         #celldata={"pressure":p, "maxLambda":Model.maxLambda(0,0,u_h,as_vector([1,0]))},
@@ -68,7 +73,7 @@ def useODESolver(polOrder=2, limiter='default'):
         number=count, subsampling=2)
     start = time.time()
     tcount = 0
-    while t < endTime:
+    while t < Model.endTime:
         ode.solve(u_h)
         # operator.applyLimiter( u_h );
         dt = ode.deltaT()
@@ -79,7 +84,7 @@ def useODESolver(polOrder=2, limiter='default'):
             print('[',tcount,']','dt = ', dt, 'time = ',t, 'count = ',count, flush=True )
         if t > saveTime:
             count += 1
-            grid.writeVTK(name,
+            grid.writeVTK(Model.name,
                 pointdata=[u_h],
                 #celldata={"pressure":p, "maxLambda":Model.maxLambda(0,0,u_h,as_vector([1,0]))},
                 #cellvector={"velocity":v},
@@ -87,18 +92,18 @@ def useODESolver(polOrder=2, limiter='default'):
             saveTime += saveStep
     print("time loop:",time.time()-start)
     print("number of time steps ", tcount)
-    grid.writeVTK(name,
+    grid.writeVTK(Model.name,
         pointdata=[u_h],
         #celldata={"pressure":p, "maxLambda":Model.maxLambda(0,0,u_h,as_vector([1,0]))},
         #cellvector={"velocity":v},
         number=count, subsampling=2)
 
-scheme = 2
+scheme = 1
 
 if scheme == 0:
     # grid = structuredGrid(x0,x1,N)
-    grid = create.grid("ALUSimplex", cartesianDomain(x0,x1,N))
-    #grid = create.grid("ALUCube", cartesianDomain(x0,x1,N))
+    #grid = create.grid("ALUSimplex", cartesianDomain(x0,x1,N))
+    grid = create.grid("ALUCube", cartesianDomain(x0,x1,N))
     grid.hierarchicalGrid.globalRefine(1)
     # grid = create.view("adaptive", grid)
     useODESolver(2,'default')      # third order with limiter
@@ -111,8 +116,8 @@ elif scheme == 1:
     # grid = create.grid("ALUSimplex", cartesianDomain(x0,x1,N))
     useODESolver(0,None)           # FV scheme
 elif scheme == 2:
-    grid = create.grid("ALUSimplex", cartesianDomain(x0,x1,N))
-    #grid = create.grid("ALUCube", cartesianDomain(x0,x1,N))
+    #grid = create.grid("ALUSimplex", cartesianDomain(x0,x1,N))
+    grid = create.grid("ALUCube", cartesianDomain(x0,x1,N))
     grid.hierarchicalGrid.globalRefine(1)
     #N = [n*6 for n in N]
     #grid = structuredGrid(x0,x1,N)
