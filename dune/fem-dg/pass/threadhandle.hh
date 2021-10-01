92e68922163864f41b0f8bef027deb3438ef3870
dedner <a.s.dedner@warwick.ac.uk>
2021-10-01 10:27:01 +0100

    adapt code to moving threadmanager/pool to mpimanager - setting the max thread number still needs to be sorted out

--- dune/fem-dg/pass/threadhandle.hh
+++ dune/fem-dg/pass/threadhandle.hh
@@ -1,14 +1,14 @@
 #ifndef DUNE_FEM_PTHREADCLASS_HH
 #define DUNE_FEM_PTHREADCLASS_HH
-#warning "Deprecated header, use #include <dune/fem/misc/threads/threadpool.hh> instead!"
-#include <dune/fem/misc/threads/threadpool.hh>
+#warning "Deprecated header, use #include <dune/fem/misc/mpimanager.hh> instead!"
+#include <dune/fem/misc/mpimanager.hh>
 
 namespace Dune
 {
   namespace Fem
   {
     //! deprecated typedef, use ThreadPool
-    typedef ThreadPool ThreadPool;
+    typedef MPIManager ThreadPool;
   }
 }
 #endif
